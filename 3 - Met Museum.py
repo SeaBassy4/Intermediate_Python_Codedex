23b554a349575fca784e7e0dec2d1f40c9143551
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-02 09:32:35 -0700

    Create 3 - Met Museum.py

---
+++ 3 - Met Museum.py
@@ -0,0 +1,16 @@
+# Creating a dictionary
+art_piece = {
+  'name': 'The Death of Socrates',
+  'artist': ' Jacques Louis David',
+  'date': '1787',
+  'period': 'Classical'
+  }
+
+# Accessing dictionary elements
+print('Name:', art_piece['name'])
+print('Artist:', art_piece['artist'])
+print('date:', art_piece['date'])
+
+# Modifying dictionary elements
+art_piece['period'] = 'Neoclassical'
+print('Updated Period:', art_piece['period'])
