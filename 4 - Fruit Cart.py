8fc77060b84697376c5f53f95864acb80368b802
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-02 09:45:09 -0700

    4 - Fruit Cart

---
+++ 4 - Fruit Cart.py
@@ -0,0 +1,16 @@
+list1 = {'🍎 apple', '🍌 banana', '🍊 orange', '🍓 strawberries'}
+list2 = {'🍒 cherries', '🫐 blueberries', '🍇 grapes', '🍓 strawberries'}
+
+# Union of sets
+union_result = list1.union(list2)
+
+# Intersection of sets
+intersection_result = list1.intersection(list2)
+
+# Difference of sets
+difference_result = list1.difference(list2)
+
+print(union_result)
+print(intersection_result)
+print(difference_result)
+
