98cedefafd1f4a8f644b1b269e1f703a37dcb946
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-05 08:48:42 -0700

    6 7

--- 6 - Dear Diary/6 - Dear Diary.py
+++ 6 - Dear Diary/6 - Dear Diary.py
@@ -1,2 +1,7 @@
-file = open('example.txt', 'w',  encoding='utf-8')
-file.write('Hello, World! 🌎')
+file = open('example.txt', 'r',  encoding='utf-8')
+lines = file.readlines()
+
+for line in lines:
+  print(line, end='')
+  file.close()
+  
